f9be936f231c893a38bbdd28913e451e0721dfd0
Vladimir Ilin <xrigikx@mail.ru>
2018-05-04 22:30:36 +0200

    added custom scripts and configs

--- lib/datasets/dataset_catalog.py
+++ lib/datasets/dataset_catalog.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##############################################################################
-
 """Collection of available datasets."""
 
 from __future__ import absolute_import
@@ -21,7 +20,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
-
 
 # Path to data dir
 _DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
@@ -39,151 +37,225 @@ RAW_DIR = 'raw_dir'
 DATASETS = {
     'cityscapes_fine_instanceonly_seg_train': {
         IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
+        _DATA_DIR + '/cityscapes/images',
         ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_train.json',
+        _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_train.json',
         RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
+        _DATA_DIR + '/cityscapes/raw'
     },
     'cityscapes_fine_instanceonly_seg_val': {
         IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
+        _DATA_DIR + '/cityscapes/images',
         # use filtered validation as there is an issue converting contours
         ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_filtered_gtFine_val.json',
+        _DATA_DIR +
+        '/cityscapes/annotations/instancesonly_filtered_gtFine_val.json',
         RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
+        _DATA_DIR + '/cityscapes/raw'
     },
     'cityscapes_fine_instanceonly_seg_test': {
         IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
+        _DATA_DIR + '/cityscapes/images',
         ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_test.json',
+        _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_test.json',
         RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
+        _DATA_DIR + '/cityscapes/raw'
     },
     'coco_2014_train': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_train2014.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_train2014',
+        ANN_FN: _DATA_DIR + '/coco/annotations/instances_train2014.json'
     },
     'coco_2014_val': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_val2014.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_val2014',
+        ANN_FN: _DATA_DIR + '/coco/annotations/instances_val2014.json'
     },
     'coco_2014_minival': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_minival2014.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_val2014',
+        ANN_FN: _DATA_DIR + '/coco/annotations/instances_minival2014.json'
     },
     'coco_2014_valminusminival': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
+        IM_DIR: _DATA_DIR + '/coco/coco_val2014',
         ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_valminusminival2014.json'
+        _DATA_DIR + '/coco/annotations/instances_valminusminival2014.json'
     },
     'coco_2015_test': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2015.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_test2015',
+        ANN_FN: _DATA_DIR + '/coco/annotations/image_info_test2015.json'
     },
     'coco_2015_test-dev': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_test2015',
+        ANN_FN: _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
     },
     'coco_2017_test': {  # 2017 test uses 2015 test images
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2017.json',
-        IM_PREFIX:
-            'COCO_test2015_'
+        IM_DIR: _DATA_DIR + '/coco/coco_test2015',
+        ANN_FN: _DATA_DIR + '/coco/annotations/image_info_test2017.json',
+        IM_PREFIX: 'COCO_test2015_'
     },
     'coco_2017_test-dev': {  # 2017 test-dev uses 2015 test images
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2017.json',
-        IM_PREFIX:
-            'COCO_test2015_'
+        IM_DIR: _DATA_DIR + '/coco/coco_test2015',
+        ANN_FN: _DATA_DIR + '/coco/annotations/image_info_test-dev2017.json',
+        IM_PREFIX: 'COCO_test2015_'
     },
     'coco_stuff_train': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/coco_stuff_train.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_train2014',
+        ANN_FN: _DATA_DIR + '/coco/annotations/coco_stuff_train.json'
     },
     'coco_stuff_val': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/coco_stuff_val.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_val2014',
+        ANN_FN: _DATA_DIR + '/coco/annotations/coco_stuff_val.json'
     },
     'keypoints_coco_2014_train': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_train2014.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_train2014',
+        ANN_FN: _DATA_DIR + '/coco/annotations/person_keypoints_train2014.json'
     },
     'keypoints_coco_2014_val': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_val2014.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_val2014',
+        ANN_FN: _DATA_DIR + '/coco/annotations/person_keypoints_val2014.json'
     },
     'keypoints_coco_2014_minival': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
+        IM_DIR: _DATA_DIR + '/coco/coco_val2014',
         ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_minival2014.json'
+        _DATA_DIR + '/coco/annotations/person_keypoints_minival2014.json'
     },
     'keypoints_coco_2014_valminusminival': {
         IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
+        _DATA_DIR + '/coco/coco_val2014',
         ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_valminusminival2014.json'
+        _DATA_DIR +
+        '/coco/annotations/person_keypoints_valminusminival2014.json'
     },
     'keypoints_coco_2015_test': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2015.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_test2015',
+        ANN_FN: _DATA_DIR + '/coco/annotations/image_info_test2015.json'
     },
     'keypoints_coco_2015_test-dev': {
-        IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
+        IM_DIR: _DATA_DIR + '/coco/coco_test2015',
+        ANN_FN: _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
     },
     'voc_2007_trainval': {
-        IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_trainval.json',
-        DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
+        IM_DIR: _DATA_DIR + '/VOC2007/JPEGImages',
+        ANN_FN: _DATA_DIR + '/VOC2007/annotations/voc_2007_trainval.json',
+        DEVKIT_DIR: _DATA_DIR + '/VOC2007/VOCdevkit2007'
     },
     'voc_2007_test': {
-        IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_test.json',
-        DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
+        IM_DIR: _DATA_DIR + '/VOC2007/JPEGImages',
+        ANN_FN: _DATA_DIR + '/VOC2007/annotations/voc_2007_test.json',
+        DEVKIT_DIR: _DATA_DIR + '/VOC2007/VOCdevkit2007'
     },
     'voc_2012_trainval': {
+        IM_DIR: _DATA_DIR + '/VOC2012/JPEGImages',
+        ANN_FN: _DATA_DIR + '/VOC2012/annotations/voc_2012_trainval.json',
+        DEVKIT_DIR: _DATA_DIR + '/VOC2012/VOCdevkit2012'
+    },
+    'hardhats_coco_2014_train': {
         IM_DIR:
-            _DATA_DIR + '/VOC2012/JPEGImages',
+        _DATA_DIR + '/hardhats_coco/hardhats_coco_train2014',
         ANN_FN:
-            _DATA_DIR + '/VOC2012/annotations/voc_2012_trainval.json',
-        DEVKIT_DIR:
-            _DATA_DIR + '/VOC2012/VOCdevkit2012'
+        _DATA_DIR +
+        '/hardhats_coco/annotations/instances_train2014.json'
+    },
+    'hardhats_coco_2014_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_coco/hardhats_coco_val2014',
+        ANN_FN:
+        _DATA_DIR + '/hardhats_coco/annotations/instances_val2014.json'
+    },
+    'hardhats_filters_coco_2014_train': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_filters_coco/train',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_filters_coco/annotations/instances_train2014.json'
+    },
+    'hardhats_filters_coco_2014_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_filters_coco/val',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_filters_coco/annotations/instances_val2014.json'
+    },
+    'hardhats_4_train': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_filters_coco_4/train/train',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_filters_coco_4/annotations/instances_train2014.json'
+    },
+    'hardhats_4_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_filters_coco_4/val',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_filters_coco_4/annotations/instances_val2014.json'
+    },
+    'hardhats_persons_train': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons/train/train',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons/annotations/instances_train2014.json'
+    },
+    'hardhats_persons_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons/val',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons/annotations/instances_val2014.json'
+    },
+    'hardhats_persons_2_train': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_2/train',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_2/annotations/instances_train2014.json'
+    },
+    'hardhats_persons_2_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_2/val',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_2/annotations/instances_val2014.json'
+    },
+    'hardhats_persons_3_train': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_3/train',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_3/annotations/instances_train2014.json'
+    },
+    'hardhats_persons_3_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_3/val',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_3/annotations/instances_val2014.json'
+    },
+    'hardhats_persons_4_train': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_4/train',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_4/annotations/instances_train2014.json'
+    },
+    'hardhats_persons_4_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_4/val',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_4/annotations/instances_val2014.json'
+    },
+    'hardhats_persons_5_train': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_5/train/train',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_5/annotations/instances_train2014.json'
+    },
+    'hardhats_persons_5_val': {
+        IM_DIR:
+        _DATA_DIR + '/hardhats_persons_5/val',
+        ANN_FN:
+        _DATA_DIR +
+        '/hardhats_persons_5/annotations/instances_val2014.json'
     }
 }
---
+++ tools/custom/infer_simple_csv.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python2
+
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+"""Perform inference on a single image or all images with a certain extension
+(e.g., .jpg) in a folder.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import defaultdict
+from sympy import *
+import argparse
+import cv2  # NOQA (Must import before importing caffe2 due to bug in cv2)
+import glob
+import logging
+import os
+import sys
+import time
+import json
+
+from caffe2.python import workspace
+from pdb import set_trace
+
+from core.config import assert_and_infer_cfg
+from core.config import cfg
+from core.config import merge_cfg_from_file
+from utils.timer import Timer
+import core.test_engine as infer_engine
+import datasets.dummy_datasets as dummy_datasets
+import utils.c2 as c2_utils
+import utils.logging
+import utils.vis as vis_utils
+
+person_class_index = 1
+hardhat_class_index = 2
+
+CHANCE_THRESHOLD = 0.7
+
+c2_utils.import_detectron_ops()
+# OpenCL may be enabled by default in OpenCV3; disable it because it's not
+# thread safe and causes unwanted GPU memory allocations.
+cv2.ocl.setUseOpenCL(False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='End-to-end inference')
+    parser.add_argument(
+        '--cfg',
+        dest='cfg',
+        help='cfg model file (/path/to/model_config.yaml)',
+        default=None,
+        type=str)
+    parser.add_argument(
+        '--wts',
+        dest='weights',
+        help='weights model file (/path/to/model_weights.pkl)',
+        default=None,
+        type=str)
+    parser.add_argument(
+        '--output-dir',
+        dest='output_dir',
+        help='directory for visualization pdfs (default: /tmp/infer_simple)',
+        default='/tmp/infer_simple',
+        type=str)
+    parser.add_argument(
+        '--image-ext',
+        dest='image_ext',
+        help='image file name extension (default: jpg)',
+        default='jpg',
+        type=str)
+    parser.add_argument(
+        '--csv-path',
+        dest='csv_path',
+        help='path to csv file',
+        default='/tmp/data.csv',
+        type=str)
+    parser.add_argument(
+        'im_or_folder', help='image or folder of images', default=None)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return parser.parse_args()
+
+
+def create_polygon(cords):
+    left_top = (cords[0], cords[1])
+    left_bottom = (cords[0], cords[3])
+    right_top = (cords[2], cords[1])
+    right_bottom = (cords[2], cords[3])
+    return Polygon(left_bottom, left_top, right_top, right_bottom)
+
+
+def intersects(person, hardhat):
+    intersection = person.intersection(hardhat)
+    return len(intersection) != 0 or person.encloses_point(hardhat.vertices[0])
+
+
+def find_intruders(persons, hardhats):
+    per_pols = [create_polygon(p) for p in persons]
+    hat_pols = [create_polygon(h) for h in hardhats]
+
+    pers_without_hats = []
+
+    for p in range(len(persons)):
+        per_with_hat = False
+        for h in range(len(hardhats)):
+            per_with_hat = per_with_hat or intersects(per_pols[p], hat_pols[h])
+        if not per_with_hat:
+            pers_without_hats.append(persons[p])
+
+    return pers_without_hats
+
+
+def get_class_probability(bbox_coord):
+    return bbox_coord[4]
+
+
+def main(args):
+    logger = logging.getLogger(__name__)
+    merge_cfg_from_file(args.cfg)
+    cfg.TEST.WEIGHTS = args.weights
+    cfg.NUM_GPUS = 1
+    assert_and_infer_cfg()
+    model = infer_engine.initialize_model_from_cfg(args.weights)
+    dummy_coco_dataset = dummy_datasets.get_coco_dataset()
+
+    if os.path.isdir(args.im_or_folder):
+        im_list = glob.iglob(args.im_or_folder + '/*.' + args.image_ext)
+    else:
+        im_list = [args.im_or_folder]
+
+    im_list = list(im_list)
+    im_list.sort()
+    json_output = []
+
+    csv = open(args.csv_path, 'w+')
+
+    for i, im_name in enumerate(im_list):
+        out_name = os.path.join(
+            args.output_dir, '{}'.format(os.path.basename(im_name) + '.pdf'))
+
+        im = cv2.imread(im_name)
+
+        logger.info('Processing {} -> {}'.format(im_name, out_name))
+
+        with c2_utils.NamedCudaScope(0):
+            cls_boxes, _, _ = infer_engine.im_detect_all(model, im, None)
+
+        boxes, _, _, classes = vis_utils.convert_from_cls_format(
+            cls_boxes, None, None)
+
+        if boxes is None:
+            boxes = []
+        else:
+            boxes = boxes.tolist()
+
+        persons = []
+        hardhats = []
+
+        for person in cls_boxes[person_class_index]:
+            if person[4] > CHANCE_THRESHOLD:
+                persons.append(person)
+
+        for hardhat in cls_boxes[hardhat_class_index]:
+            if hardhat[4] > CHANCE_THRESHOLD:
+                hardhats.append(hardhat)
+
+        intruder = find_intruders(persons, hardhats)
+
+        if len(intruder) != 0:
+            file_name = os.path.split(im_name)[1]
+            file_without_ext = os.path.splitext(file_name)[0]
+            csv.write("TEXT," + str(file_without_ext) + "," +
+                      str(file_without_ext) + ".pdf" + '\n')
+
+            vis_utils.vis_one_image(
+                im[:, :, ::-1],  # BGR -> RGB for visualization
+                file_without_ext,
+                args.output_dir,
+                cls_boxes,
+                None,
+                None,
+                dataset=dummy_coco_dataset,
+                box_alpha=0.3,
+                show_class=True,
+                thresh=0.7,
+                kp_thresh=2)
+    csv.close()
+
+
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    utils.logging.setup_logging(__name__)
+    args = parse_args()
+    print(args)
+    main(args)
